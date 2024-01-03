61ca53d675102ad6670b0aa596b3047fb4d29144
CraigYeoman <yeomancraig@gmail.com>
2024-01-03 13:00:25 -0600

    Fix for github pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/karlaleland" element={<Home />}></Route>
       </Routes>
       <Footer />
     </Router>
